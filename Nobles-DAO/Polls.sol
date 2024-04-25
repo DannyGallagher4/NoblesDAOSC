945dbb525d11bcd659a481e2b9d528aa1304c845
klodan <dgallagher24@nobles.edu>
2024-04-25 14:44:47 -0400

    poll functions

--- Nobles-DAO/Polls.sol
+++ Nobles-DAO/Polls.sol
@@ -3,20 +3,53 @@ pragma solidity >=0.8.2 <0.9.0;
 contract Polls{
 
     address public userContractAddress;
+    uint counter = 1;
+
+    struct Choice{
+        string option;
+        address[] votes;
+    }
 
     struct Poll{
         uint id;
         string name;
-
-        //these next 2 lines are current placeholders for the better way we will store votes and choices
-        string[] choices;
-        mapping(string => address[]) votes;
+        Choice[] choices;
     }
 
-    constructor(address _userContractAddress){
-        userContractAddress = _userContractAddress;
+    Poll[] activePolls;
+
+    function containsAddress(address[] memory myArray, address _value) public pure returns (bool){
+        for (uint i = 0; i < myArray.length; i++) {
+            if (myArray[i] == _value) {
+                return true;
+            }
+        }
+        return false;
     }
 
+    function createPoll(address[] memory teachers, address originalCaller, string memory question, string[] memory options) public {
+        require(containsAddress(teachers, originalCaller));
+        Choice[] memory myChoices = new Choice[](options.length);
+        for(uint i = 0; i < options.length; i++){
+            address[] memory myaddr;
+            myChoices[i] = Choice(options[i], myaddr);
+        }
+        activePolls.push(Poll(counter, question, myChoices));
+        counter++;
+    }
+
+    function vote(address[] memory users, address originalCaller, uint pollId, uint optionIndex) public{
+        require(containsAddress(users, originalCaller));
+        for(uint i = 0; i < activePolls.length; i++){
+            if(activePolls[i].id == pollId){
+                for(uint j = 0; j < activePolls[i].choices.length; j++){
+                    require(!containsAddress(activePolls[i].choices[j].votes, originalCaller), "User already voted");
+                }
+                activePolls[i].choices[optionIndex].votes.push(originalCaller);
+                break;
+            }
+        }
+    }
 
 
 }
