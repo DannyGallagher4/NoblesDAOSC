17d447eb9904245a5b8aa896be4ae72947e79683
James Strik <jamesbstrik@gmail.com>
2024-05-01 11:37:34 -0400

    viewPolls function added

--- Nobles-DAO/NoblesDAO.sol
+++ Nobles-DAO/NoblesDAO.sol
@@ -98,4 +98,9 @@ contract NoblesDAO{
         }
     }
 
+    function viewPolls(address originalCaller) public view returns (Poll[] memory) {
+        require(containsAddress(students, originalCaller) || containsAddress(teachers, originalCaller) || containsAddress(admins, originalCaller));
+        return activePolls;
+    }
+
 }
